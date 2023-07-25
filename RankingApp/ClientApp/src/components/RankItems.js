5eb39936f11fce51d50a354185c840f8f63a502f
Alaina Balducci <alainabalducci@gmail.com>
2023-07-25 14:25:36 -0500

    imported movieImages to RankItems.js and used the find method to populate the page with the images

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
 
 const RankItems = () => {
 
@@ -20,9 +21,13 @@ const RankItems = () => {
      // Conditional rendering: If items are available, display their titles
     return (
         <main>
+        <div class = "items-not=ranked">
             {
-                (items.length > 0) ? items.map((item) => <h3>{item.title}</h3>): <div>Loading...</div>
-            }
+                    (items.length > 0) ? items.map((item) =>
+                        <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id === item.imageId)?.image} />
+                        ) : <div>Loading...</div>
+                }
+            </div>
         </main>
     )
 }
