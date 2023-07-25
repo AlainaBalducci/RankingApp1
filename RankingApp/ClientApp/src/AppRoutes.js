42b69b4731bdbe253cf62146b6dc56455291d8e8
Alaina Balducci <alainabalducci@gmail.com>
2023-07-25 13:17:03 -0500

    pointed AppRoute to RankItems component

--- RankingApp/ClientApp/src/AppRoutes.js
+++ RankingApp/ClientApp/src/AppRoutes.js
@@ -1,20 +1,26 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import RankItems from "./components/RankItems";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: '/counter',
+        element: <Counter />
+    },
+    //point to new RankItems component array
+    {
+        path: '/rank-items',
+        element: <RankItems />
+    },
+    {
+        path: '/fetch-data',
+        element: <FetchData />
+    }
 ];
 
 export default AppRoutes;
