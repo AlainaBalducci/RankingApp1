4f501f0ba610d4cbb43f017fa7df1c974153c015
Alaina Balducci <alainabalducci@gmail.com>
2023-07-25 13:10:57 -0500

    added comments to react component

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -5,16 +5,19 @@ const RankItems = () => {
     const [items, setItems] = useState([]);
     const dataType = 1;
 
+     // Effect hook to fetch data from the API when the component is mounted or dataType changes
     useEffect(() => {
+         // Fetch data from the API using the specified dataType 
         fetch(`item/${dataType}`)
             .then((results) => {
-                return results.json();
+                return results.json();  // Convert the response to JSON format
             })
             .then(data => {
-                setItems(data);
+                setItems(data);  // Update the items state with the fetched data
             })
     }, []);
 
+     // Conditional rendering: If items are available, display their titles
     return (
         <main>
             {
